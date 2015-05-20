63f37088aa8499082f858636cdd7dc50fd8bb120
Winns/Pavel <buhhcz@gmail.com>
2015-05-20 11:09:44 +0300

    upd

--- js/psdev.cart.js
+++ js/psdev.cart.js
@@ -1,245 +1,244 @@
 'use strict';
 
 var psdevCart = function( cfg ) {
-	var self = this;
+    var self = this;
 
-	this.cfg = $.extend({
-		elAdd			: '.js-cart-add',
-		elRemove		: '.js-cart-remove',
-		elClear			: '.js-cart-clear',
-		elPlus			: '.js-cart-plus',
-		elMinus			: '.js-cart-minus',
-		elCountInput	: '.js-cart-count',
+    this.cfg = $.extend({
+        elAdd           : '.js-cart-add',
+        elRemove        : '.js-cart-remove',
+        elClear         : '.js-cart-clear',
+        elPlus          : '.js-cart-plus',
+        elMinus         : '.js-cart-minus',
+        elCountInput    : '.js-cart-count',
 
-		syncInterval	: 1500,
-		constructor		: null
-	}, cfg || {});
+        syncInterval    : 1500,
+        constructor     : null
+    }, cfg || {});
 
-	this.updateTime = null;
+    this.updateTime = null;
 
-	this.msg = {
-		ERR_DATAID: function( source ) {
-			return 'psdevCart ("'+ source +'") - Element dont have data-id attribute';
-		}
-	};
+    this.msg = {
+        ERR_DATAID: function( source ) {
+            return 'psdevCart ("'+ source +'") - Element dont have data-id attribute';
+        }
+    };
 
-	// On
-	this.onPool = [];
+    // On
+    this.onPool = [];
 
-	this.on = function( method, f ) {
-		this.onPool.push({
-			method: method,
-			f: f
-		});
-	};
+    this.on = function( method, f ) {
+        this.onPool.push({
+            method: method,
+            f: f
+        });
+    };
 
-	this.onTrigger = function( method, data ) {
-		var o, data = data || null;
+    this.onTrigger = function( method, data ) {
+        var o, data = data || null;
 
-		for (var i=0; i < this.onPool.length; i++) {
-			o = this.onPool[i];
-			if (o.method === method) o.f( data );
-		}
-	};
-	// end On
+        for (var i=0; i < this.onPool.length; i++) {
+            o = this.onPool[i];
+            if (o.method === method) o.f( data );
+        }
+    };
+    // end On
 
-	this.add = function( data ) {
-		var items = this.load().items;
+    this.add = function( data ) {
+        var items = this.load().items;
 
-		if ( items.hasOwnProperty( data.id ) ) return;
+        if ( items.hasOwnProperty( data.id ) ) return;
 
-		items[ data.id ] = data;
+        items[ data.id ] = data;
 
-		this.save( items );
-		this.onTrigger( 'add', data );
-	};
+        this.save( items );
+        this.onTrigger( 'add', data );
+    };
 
-	this.remove = function( id ) {
-		var items = this.load().items;
+    this.remove = function( id ) {
+        var items = this.load().items;
 
-		if ( !items.hasOwnProperty( id ) ) return;
+        if ( !items.hasOwnProperty( id ) ) return;
 
-		delete items[ id ];
+        delete items[ id ];
 
-		this.save( items );
-		this.onTrigger( 'remove', id );
-	};
+        this.save( items );
+        this.onTrigger( 'remove', id );
+    };
 
-	this.clear = function() {
-		this.save({});
-		this.onTrigger( 'clear' );
-	};
+    this.clear = function() {
+        this.save({});
+        this.onTrigger( 'clear' );
+    };
 
-	this.update = function( id, data ) {
-		var items = this.load().items;
+    this.update = function( id, data ) {
+        var items = this.load().items;
 
-		if (items.hasOwnProperty( id )) {
-			items[ id ] = $.extend( items[ id ], data );
-			this.save( items );
-		} else {
-			this.add({ id: id, count: 1 });
-		}
-	};
+        if (items.hasOwnProperty( id )) {
+            items[ id ] = $.extend( items[ id ], data );
+            this.save( items );
+        } else {
+            this.add({ id: id, count: 1 });
+        }
+    };
 
-	this.get = function( id ) {
-		var items = this.load().items;
+    this.get = function( id ) {
+        var items = this.load().items;
 
-		if (items.hasOwnProperty( id ))
-			return items[ id ]
-		else
-			return false;
-	};
+        if (items.hasOwnProperty( id ))
+            return items[ id ]
+        else
+            return false;
+    };
 
-	this.render = function() {
-		this.onTrigger( 'render', this.load() );
-	};
+    this.render = function() {
+        this.onTrigger( 'render', this.load() );
+    };
 
-	// Stotage methods
-	this.fixStorage = function() {
-		var data = localStorage.getItem( 'cart' ),
-			cart = JSON.stringify({
-				updateTime: new Date().getTime(),
-				items: {},
-				count: 0
-			});
+    // Stotage methods
+    this.fixStorage = function() {
+        var data = localStorage.getItem( 'cart' ),
+            cart = JSON.stringify({
+                updateTime: new Date().getTime(),
+                items: {},
+                count: 0
+            });
 
-		try {
-			if (data === null) {
-				localStorage.setItem( 'cart', cart );
-			} else {
-				data = JSON.parse( data );
+        try {
+            if (data === null) {
+                localStorage.setItem( 'cart', cart );
+            } else {
+                data = JSON.parse( data );
 
-				if ((!data.hasOwnProperty( 'updateTime' )) || (!data.hasOwnProperty( 'items' )) || (!data.hasOwnProperty( 'count' ))) {
-					localStorage.setItem( 'cart', cart );
-				}
-			}
-		} catch(e) {
-			localStorage.setItem( 'cart', cart );
-		}
-	};
+                if ((!data.hasOwnProperty( 'updateTime' )) || (!data.hasOwnProperty( 'items' )) || (!data.hasOwnProperty( 'count' ))) {
+                    localStorage.setItem( 'cart', cart );
+                }
+            }
+        } catch(e) {
+            localStorage.setItem( 'cart', cart );
+        }
+    };
 
-	this.load = function() {
-		this.fixStorage();
-		return JSON.parse( localStorage.getItem( 'cart' ) );
-	};
+    this.load = function() {
+        this.fixStorage();
+        return JSON.parse( localStorage.getItem( 'cart' ) );
+    };
 
-	this.save = function( items ) {
-		var o = {
-			updateTime: new Date().getTime(),
-			items: items,
-			count: (function() {
-				var n = 0;
+    this.save = function( items ) {
+        var o = {
+            updateTime: new Date().getTime(),
+            items: items,
+            count: (function() {
+                var n = 0;
 
-				for (var key in items) {
-					n += items[ key ].count >> 0;
-				}
+                for (var key in items) {
+                    n += items[ key ].count >> 0;
+                }
 
-				return n;
-			})()
-		};
-		this.updateTime = o.updateTime;
+                return n;
+            })()
+        };
+        this.updateTime = o.updateTime;
 
-		localStorage.setItem( 'cart', JSON.stringify( o ) );
+        localStorage.setItem( 'cart', JSON.stringify( o ) );
 
-		this.onTrigger( 'save', items );
-		this.onTrigger( 'render', o );
-	};
-	// END Stotage methods
+        this.onTrigger( 'save', items );
+        this.onTrigger( 'render', o );
+    };
+    // END Stotage methods
 
-	// Init
-	this.init = function() {
-		var data = this.load();
+    // Init
+    this.init = function() {
+        var data = this.load();
 
-		if (this.cfg.constructor !== null)
-			this.cfg.constructor( this, data );
+        if (this.cfg.constructor !== null)
+            this.cfg.constructor( this, data );
 
-		this.updateTime = data.updateTime;
-		this.onTrigger( 'render', data );
+        this.updateTime = data.updateTime;
+        this.onTrigger( 'render', data );
 
-		// Start localstorage auto sync
-		setInterval(function() {
-			var data = self.load();
+        // Start localstorage auto sync
+        setInterval(function() {
+            var data = self.load();
 
-			if (data.updateTime != self.updateTime) {
-				self.updateTime = data.updateTime;
-				self.onTrigger( 'render', self.load() );
-				self.get(45);
-			}
-		}, self.cfg.syncInterval );
+            if (data.updateTime != self.updateTime) {
+                self.updateTime = data.updateTime;
+                self.onTrigger( 'render', self.load() );
+            }
+        }, self.cfg.syncInterval );
 
-		// On Add click
-		$( document ).on( 'click', this.cfg.elAdd, function() {
-			var id		= $( this ).attr( 'data-id' ),
-				oldData	= self.get( id );
+        // On Add click
+        $( document ).on( 'click', this.cfg.elAdd, function() {
+            var id      = $( this ).attr( 'data-id' ),
+                oldData = self.get( id );
 
-			if (id === undefined) 
-				throw new Error( self.msg['ERR_DATAID']('On Add click') );
+            if (id === undefined) 
+                throw new Error( self.msg['ERR_DATAID']('On Add click') );
 
-			if (!oldData) self.add({ id: id, count: 1 });
-		});
+            if (!oldData) self.add({ id: id, count: 1 });
+        });
 
-		// On Remove click
-		$( document ).on( 'click', this.cfg.elRemove, function() {
-			var id = $( this ).attr( 'data-id' );
+        // On Remove click
+        $( document ).on( 'click', this.cfg.elRemove, function() {
+            var id = $( this ).attr( 'data-id' );
 
-			if (id === undefined) 
-				throw new Error( self.msg['ERR_DATAID']('On Remove click') );
+            if (id === undefined) 
+                throw new Error( self.msg['ERR_DATAID']('On Remove click') );
 
-			self.remove( id );
-		});
+            self.remove( id );
+        });
 
-		// On Plus click
-		$( document ).on( 'click', this.cfg.elPlus, function() {
-			var id		= $( this ).attr( 'data-id' ),
-				oldData = self.get( id );
+        // On Plus click
+        $( document ).on( 'click', this.cfg.elPlus, function() {
+            var id      = $( this ).attr( 'data-id' ),
+                oldData = self.get( id );
 
-			if (id === undefined) 
-				throw new Error( self.msg['ERR_DATAID']('On Plus click') );
+            if (id === undefined) 
+                throw new Error( self.msg['ERR_DATAID']('On Plus click') );
 
-			if (!oldData)
-				self.add({ id: id, count: 1 });
-			else
-				self.update( id, {count: oldData.count + 1} );
-		});
+            if (!oldData)
+                self.add({ id: id, count: 1 });
+            else
+                self.update( id, {count: oldData.count + 1} );
+        });
 
-		// On Minus click
-		$( document ).on( 'click', this.cfg.elMinus, function() {
-			var id		= $( this ).attr( 'data-id' ),
-				oldData = self.get( id ),
-				limit	= $( this ).attr( 'data-limit' );
+        // On Minus click
+        $( document ).on( 'click', this.cfg.elMinus, function() {
+            var id      = $( this ).attr( 'data-id' ),
+                oldData = self.get( id ),
+                limit   = $( this ).attr( 'data-limit' );
 
-			if (id === undefined) 
-				throw new Error( self.msg['ERR_DATAID']('On Minus click') );
+            if (id === undefined) 
+                throw new Error( self.msg['ERR_DATAID']('On Minus click') );
 
-			if (limit === undefined)
-				limit = 0;
-			else
-				limit = limit >> 0;
+            if (limit === undefined)
+                limit = 0;
+            else
+                limit = limit >> 0;
 
-			if (oldData && oldData.count > limit) {
-				if ((oldData.count - 1) <= 0)
-					self.remove( id );
-				else
-					self.update( id, {count: oldData.count - 1} );
-			}
-		});
+            if (oldData && oldData.count > limit) {
+                if ((oldData.count - 1) <= 0)
+                    self.remove( id );
+                else
+                    self.update( id, {count: oldData.count - 1} );
+            }
+        });
 
-		// On Count Input change
-		$( document ).on( 'change', this.cfg.elCountInput, function() {
-			var id 	= $( this ).attr( 'data-id' ),
-				val = $( this ).val() >> 0;
+        // On Count Input change
+        $( document ).on( 'change', this.cfg.elCountInput, function() {
+            var id  = $( this ).attr( 'data-id' ),
+                val = $( this ).val() >> 0;
 
-			if (id === undefined) 
-				throw new Error( self.msg['ERR_DATAID']('On Count Input change') );
+            if (id === undefined) 
+                throw new Error( self.msg['ERR_DATAID']('On Count Input change') );
 
-			if ((val <= 0) || (isNaN(val))) val = 1;
+            if ((val <= 0) || (isNaN(val))) val = 1;
 
-			self.update( id, {count: val} );
-		});
+            self.update( id, {count: val} );
+        });
 
-		// On Clear click
-		$( document ).on( 'click', this.cfg.elClear, this.clear.bind(this) );
-	}
+        // On Clear click
+        $( document ).on( 'click', this.cfg.elClear, this.clear.bind(this) );
+    }
 
-	this.init();
+    this.init();
 };
